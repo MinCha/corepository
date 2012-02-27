13c7e40d674bb47de1b653d5b01214d856da13b4
Min Cha <minslovey@gmail.com>
2012-02-27 22:02:18 +0900

    Added some class comments.

--- src/main/java/com/github/writeback/client/CoRepository.java
+++ src/main/java/com/github/writeback/client/CoRepository.java
@@ -3,17 +3,28 @@ package com.github.writeback.client;
 import java.util.List;
 
 /**
- * This represents a repository which is holding intermediate values before write-back.
+ * CoRepository, core domain object, is responsible for receiving update and
+ * select requests from client instead of original repository such as database
+ * and sometimes write-backing to original repository. For this reason,
+ * CoRepository should able to rapidly process requests from client. You can
+ * read more at https://github.com/MinCha/write-back/wiki
  * 
+ * @see LocalMemoryCoRepository
  * @author Min Cha
- *
+ * 
  */
 public interface CoRepository {
 	WriteBackItem select(String key);
+
 	void update(WriteBackItem item);
+
 	void insert(WriteBackItem item);
+
 	void increase(String key);
+
 	void decrease(String key);
+
 	boolean exists(String key);
+
 	List<WriteBackItem> selectAll();
 }
--- src/main/java/com/github/writeback/client/LocalMemoryCoRepository.java
+++ src/main/java/com/github/writeback/client/LocalMemoryCoRepository.java
@@ -8,12 +8,24 @@ import java.util.Map;
 import com.github.writeback.client.exception.NonexistentKeyException;
 import com.github.writeback.client.exception.NotNumericValueException;
 
+/**
+ * <code>CoRepository</code> using local memory.
+ * 
+ * If you use this class as CoRepository, you don`t need any remote
+ * CoRepository.
+ * 
+ * <b>Warning!</b> In clustered environment such as web servers clustered by L4
+ * switch, this class does not ensure that select method returns the
+ * latest value.
+ * 
+ * @author Min Cha
+ */
 public class LocalMemoryCoRepository implements CoRepository {
-	private Map<String, Object> items = new HashMap<String, Object>(); 
-	
+	private Map<String, Object> items = new HashMap<String, Object>();
+
 	public WriteBackItem select(String key) {
 		assertThatThereIsKey(key);
-		
+
 		return new WriteBackItem(key, items.get(key));
 	}
 
@@ -25,31 +37,21 @@ public class LocalMemoryCoRepository implements CoRepository {
 
 	public void increase(String key) {
 		assertThatThereIsKey(key);
-		
+
 		Object result = items.get(key);
-		long value;
-		try {
-			value = (Long)result;
-		} catch (ClassCastException e) {
-			throw new NotNumericValueException("Cannot increase value : " + result);
-		}
-		
+		long value = covertLongFrom(result);
 		value++;
+
 		items.put(key, value);
 	}
 
 	public void decrease(String key) {
 		assertThatThereIsKey(key);
-		
+
 		Object result = items.get(key);
-		long value;
-		try {
-			value = (Long)result;
-		} catch (ClassCastException e) {
-			throw new NotNumericValueException("Cannot increase value : " + result);
-		}
-		
+		long value = covertLongFrom(result);
 		value--;
+
 		items.put(key, value);
 	}
 
@@ -69,11 +71,22 @@ public class LocalMemoryCoRepository implements CoRepository {
 
 	public List<WriteBackItem> selectAll() {
 		List<WriteBackItem> result = new ArrayList<WriteBackItem>();
-		
+
 		for (String each : items.keySet()) {
 			result.add(new WriteBackItem(each, items.get(each)));
 		}
-		
+
 		return result;
+	}
+
+	private long covertLongFrom(Object result) {
+		long value;
+		try {
+			value = (Long) result;
+		} catch (ClassCastException e) {
+			throw new NotNumericValueException("Cannot increase value : "
+					+ result);
+		}
+		return value;
 	}
 }
