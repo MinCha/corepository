d9bdb21eaa382322400239bc7af7238380d4b0bb
Min Cha <minslovey@gmail.com>
2012-03-16 22:28:16 +0900

    Renamed a root package name to corepo from writeback

--- src/main/java/com/github/writeback/client/NonExistentKeyException.java
+++
@@ -1,9 +0,0 @@
-package com.github.writeback.client;
-
-
-@SuppressWarnings("serial")
-public class NonExistentKeyException extends RuntimeException {
-	public NonExistentKeyException(String key) {
-		super("Key does not exist : " + key);
-	}
-}
--- src/main/java/com/github/writeback/client/TimeoutException.java
+++
@@ -1,8 +0,0 @@
-package com.github.writeback.client;
-
-@SuppressWarnings("serial")
-public class TimeoutException extends RuntimeException {
-	public TimeoutException(String key) {
-		super("Timeout : " + key);
-	}
-}
