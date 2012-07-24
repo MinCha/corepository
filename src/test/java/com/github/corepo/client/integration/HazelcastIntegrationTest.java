1e25e17aab6327b8a7f64899978be8367df48d21
Min Cha <minslovey@gmail.com>
2012-07-24 11:32:36 +0900

    Added a Hazelcast dependency and test

---
+++ src/test/java/com/github/corepo/client/integration/HazelcastIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.github.corepo.client.integration;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.hazelcast.core.Hazelcast;
+
+public class HazelcastIntegrationTest {
+	@Test
+	public void shouldRunAsExpected() throws InterruptedException {
+		Map<String, String> map = Hazelcast.getMap("corepo");
+		map.put("name", "min");
+		
+		assertThat(map.get("name"), is("min"));
+	}
+}
