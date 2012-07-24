1b99cfc2640a62455bbc65f3a5981d01aabff87f
Min Cha <minslovey@gmail.com>
2012-07-24 20:50:18 +0900

    Resolved https://github.com/MinCha/corepository/issues/11

--- src/main/java/com/github/corepo/client/Unlocker.java
+++ src/main/java/com/github/corepo/client/Unlocker.java
@@ -3,7 +3,11 @@ package com.github.corepo.client;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Unlocker {
+	private static final Logger LOG = LoggerFactory.getLogger(Unlocker.class);
 	private ArrayList<UnlockRequest> requests = new ArrayList<UnlockRequest>();
 	private CoRepository coRepository;
 	private boolean activation = true;
@@ -16,6 +20,7 @@ public class Unlocker {
 		Thread t= new Thread(new DeplayedUnlocker());
 		t.setDaemon(true);
 		t.start();
+		Runtime.getRuntime().addShutdownHook(new UnlockerShutdownHook());
 	}
 
 	public void requestUnlock(String key) {
@@ -30,33 +35,46 @@ public class Unlocker {
 		public void run() {
 			try {
 				while (activation) {
-					@SuppressWarnings("unchecked")
-					List<UnlockRequest> copy = (List<UnlockRequest>) requests
-							.clone();
-					for (UnlockRequest each : copy) {
-						if (each.isUnlockable() == false) {
-							continue;
-						}
-
-						if (coRepository.exists(each.key)) {
-							unlockWithRetry(each, 3);
-						}
-						requests.remove(each);
-					}
-
+					unlockAllItemsOvered();
 					Thread.sleep(100);
 				}
 			} catch (InterruptedException e) {
 			}
 		}
 
-		private void unlockWithRetry(UnlockRequest each, int retryCount) {
-			for (int i = 0; i < retryCount; i++) {
-				boolean result = coRepository.unlock(each.key);
-				if (result) {
-					return;
+		private void unlockAllItemsOvered() {
+			@SuppressWarnings("unchecked")
+			List<UnlockRequest> copy = (List<UnlockRequest>) requests
+					.clone();
+			for (UnlockRequest each : copy) {
+				if (each.isUnlockable() == false) {
+					continue;
 				}
+
+				if (coRepository.exists(each.key)) {
+					unlockWithRetry(each, 3);
+				}
+				requests.remove(each);
 			}
+		}
+	}
+
+	private void unlockWithRetry(UnlockRequest each, int retryCount) {
+		for (int i = 0; i < retryCount; i++) {
+			boolean result = coRepository.unlock(each.key);
+			if (result) {
+				return;
+			}
+		}
+	}
+
+	private class UnlockerShutdownHook extends Thread {
+		public void run() {
+			LOG.info("ShutdownHook Starts, wating count is " + requests.size());
+			for (UnlockRequest each : requests) {
+				unlockWithRetry(each, 3);
+			}
+			LOG.info("ShutdownHook ends");
 		}
 	}
 
